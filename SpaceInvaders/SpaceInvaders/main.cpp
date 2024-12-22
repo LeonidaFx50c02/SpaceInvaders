e6c369be93e0c74e3c2ec9d848bd22d36b97e618
giovanni-russo08 <giova.russo2008@gmail.com>
2024-12-22 19:55:28 +0100

    Update main.cpp
    organizzazione schermata di gioco

--- SpaceInvaders/SpaceInvaders/main.cpp
+++ SpaceInvaders/SpaceInvaders/main.cpp
@@ -1,6 +1,36 @@
-#include "immediate2d.h"
-#include <iostream>
-#include <string>
-#include <cmath>
+#define IMM2D_WIDTH 640
+#define IMM2D_HEIGHT 480
+#define IMM2D_SCALE 1
 
+#define IMM2D_IMPLEMENTATION 
+#include "immediate2d.h"
+#include <string>
 using namespace std;
+
+
+
+void run() {
+
+	int x = 0;
+
+	Clear(Yellow);//parte navicella punti bonus
+	DrawRectangle(0, 60, 640, 300, Green, Transparent);//parte nemici
+	DrawRectangle(0, 360, 640, 60, Blue, Transparent);//parte difesa
+	DrawRectangle(0, 420, 640, 60, Red, Transparent);//parte mia navicella
+	while (true) {
+		DrawRectangle(x, 450, 50, 30, Black, Transparent);//parte mia navicella
+		char caratterePremuto = LastKey();
+		if (caratterePremuto == Left)
+		{
+			x -= 10;
+		}
+		else if (caratterePremuto == Right)
+		{
+			x += 10;
+		}
+
+	}
+
+}
+
+
